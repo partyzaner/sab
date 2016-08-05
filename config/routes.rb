cb9a931e0dca3aa905720022c6d599a9378c1871
Ivan Sabaleuski <yanachak@gmail.com>
2016-08-05 20:37:30 +0300

    Devise basic configuration finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+
   resources :articles
   resources :login
   get 'landing/index'
   root 'landing#index'
+
+  devise_for :users
+  get 'persons/profile'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
