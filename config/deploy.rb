e9736445eb7f31a494556c75becad970e44b13c3
Ivan Sabaleuski <yanachak@gmail.com>
2016-09-05 15:47:39 +0300

    Deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
 # config valid only for current version of Capistrano
-lock '3.6.0'
+# lock '3.6.0'
 
 set '188.166.114.184', port: 22, roles: [:web, :app, :db], primary: true
 
-set :application, 'sab'
 set :repo_url, 'git@github.com:partyzaner/sab.git'
+set :application, 'sab'
 set :user, 'deploy'
 set :puma_threads, [4, 16]
 set :puma_workers, 0
